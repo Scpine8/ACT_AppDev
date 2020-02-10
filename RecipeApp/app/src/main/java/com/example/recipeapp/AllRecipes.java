cb24ba78da96217de8cd27fda999980f1851c61f
scpine8 <scpine8@gmail.com>
2020-02-10 18:17:35 +0200

    changes to recipes app

--- RecipeApp/app/src/main/java/com/example/recipeapp/AllRecipes.java
+++ RecipeApp/app/src/main/java/com/example/recipeapp/AllRecipes.java
@@ -3,6 +3,10 @@ package com.example.recipeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.renderscript.Sampler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AllRecipes extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class AllRecipes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_recipes);
+
+        // Configure Recipe Data
+        // (1) Beef Wellington
+
+
+
+        String wellington_title = "Beef Wellington";
+        String wellington_img_src = "@drawable/beefwellington";
+        String wellington_desc = getString(R.string.wellington_desc);
+
+        ArrayList<String> wellington = [wellington_title, wellington_img_src, wellington_desc];
+
+
     }
 }
